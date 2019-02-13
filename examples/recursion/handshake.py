d83557ee346433a184d216e6e9e7deae2300289c
Tommy McHugh <tombmch@gmail.com>
2019-02-13 13:53:21 -0600

    created function that checks if text is a palindrome

--- examples/recursion/handshake.py
+++ examples/recursion/handshake.py
@@ -1,8 +1,16 @@
+# Thomas McHugh
+# Handshake example
+
 def handshake(numOfPeople):
+	'In a party of N people, each person will shake her/his hand with each other person only once. Will return total number of hand-shakes that would happen.'
 	if numOfPeople > 1:
+		# Total shakes are the number of people minus the individual shaking
 		totalShakes = numOfPeople - 1
+		# Return the sum of shakes recursively
 		return totalShakes + handshake(numOfPeople - 1)
 	else:
+		# One person remaining
+		## Nobody to shake hand
 		return 0
 		
 print(handshake(5))
