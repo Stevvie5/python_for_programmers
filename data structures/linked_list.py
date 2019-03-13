fe729f9098820266c2575f92b8f1d00a4ca8057e
Tommy McHugh <tombmch@gmail.com>
2019-03-13 14:45:32 -0500

    added prev node for linked list

--- data structures/linked_list.py
+++ data structures/linked_list.py
@@ -1,5 +1,6 @@
 class LinkedNode(object):
 	__nextNode = None
+	__prevNode = None
 	
 	def __init__(self, nodeObject, nextObject=None):
 		self.__nodeObject = nodeObject
@@ -12,11 +13,17 @@ class LinkedNode(object):
 	def getNextNode(self):
 		return self.__nextNode
 		
+	def getPrevNode(self):
+		return self.__prevNode
+		
 	def setObject(self, nodeObject):
 		self.__nodeObject = nodeObject
 		
 	def setNextObject(self, nextObject):
 		self.__nextNode = LinkedNode(nextObject)
+		
+	def setPrevNode(self, prevNode):
+		self.__prevNode = prevNode
 
 class LinkedList(object):
 	def __init__(self, headObject):
@@ -35,23 +42,40 @@ class LinkedList(object):
 			currentNode.setNextObject(endObject)
 				
 	def next(self):
-		self.headNode = self.headNode.getNextNode()
+		nextNode = self.headNode.getNextNode()
+		nextNode.setPrevNode(self.headNode)
+		self.headNode = nextNode
 	
 	def getCurrent(self):
 		return self.headNode.getObject()
+		
+	def prev(self):
+		prevNode = self.headNode.getPrevNode()
+		self.headNode = prevNode
 
 '''
 TESTS
 
 myList = LinkedList(120)
 print(myList.getCurrent())
+
 myList.append(130)
 myList.append(150)
 myList.append(160)
+
 myList.next()
 print(myList.getCurrent())
 myList.next()
 print(myList.getCurrent())
 myList.next()
+print(myList.getCurrent())
+
+myList.prev()
+print(myList.getCurrent())
+myList.prev()
+print(myList.getCurrent())
+myList.prev()
 print(myList.getCurrent())
 '''
+
+
